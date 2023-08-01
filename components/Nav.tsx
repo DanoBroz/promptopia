67323e23b9e4be0d640caacfeddae9f26ef95704
Daniel Brož <danobroz66@gmail.com>
2023-08-01 19:41:58 +0200

    sign in with google

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,7 +14,7 @@ import {
 import { BuiltInProviderType } from "next-auth/providers";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -42,7 +42,7 @@ const Nav = () => {
 
             {/* Desktop Navigation */}
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="black_btn">
                             Create Post
@@ -85,7 +85,7 @@ const Nav = () => {
 
             {/* Mobile Navigation */}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
                             src="/assets/images/logo.svg"
