654f850e27feae2139a8415a02e04e24c7bd3bb4
Daniel Brož <danobroz66@gmail.com>
2023-08-01 09:17:50 +0200

    add mobile navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,6 +17,7 @@ const Nav = () => {
     const isUserLoggedIn = true;
 
     const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
         const fetchProviders = async () => {
@@ -73,7 +74,9 @@ const Nav = () => {
                                         type="button"
                                         onClick={() => signIn(provider.id)}
                                         className="black_btn"
-                                    ></button>
+                                    >
+                                        Sign In
+                                    </button>
                                 )
                             )}
                     </>
@@ -81,6 +84,70 @@ const Nav = () => {
             </div>
 
             {/* Mobile Navigation */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                            onClick={() =>
+                                setToggleDropdown((prevState) => !prevState)
+                            }
+                        />
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href="/create-prompt"
+                                    className="dropdown_link"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                    }}
+                                >
+                                    Create Prompt
+                                </Link>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                        signOut();
+                                    }}
+                                    className="mt-5 w-full black_btn"
+                                >
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map(
+                                (provider: ClientSafeProvider) => (
+                                    <button
+                                        key={provider.name}
+                                        type="button"
+                                        onClick={() => signIn(provider.id)}
+                                        className="black_btn"
+                                    >
+                                        Sign In
+                                    </button>
+                                )
+                            )}
+                    </>
+                )}
+            </div>
         </nav>
     );
 };
